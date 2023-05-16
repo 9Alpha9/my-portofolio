0edaf1bcebd73412d6d258eccd860dd5a142c181
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2023-05-16 16:13:19 +0700

    Remix about and styled

---
+++ src/content/AboutContent/ThisContentAbout.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function ThisContentAbout() {
+    return (
+        <>
+            <div>Sorry no content for Now. Please hold on ❤️</div>
+        </>
+    );
+}
+
+export default ThisContentAbout;
--- src/pages/AboutPages.jsx
+++ src/pages/AboutPages.jsx
@@ -1,11 +1,75 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import "../styles/AboutStyles/aboutStyles.scss";
+import styled from "styled-components";
+import { gsap } from "gsap";
+import Marquee from "react-gsap-marquee";
+import SmoothScroll from "../SmoothScroll";
+import Footer from "../components/Footer";
+
+import ThisContentAbout from "../content/AboutContent/ThisContentAbout";
 
 const AboutPages = () => {
+    const AboutWrapperContainer = styled.div`
+        @media (1201px <= width) {
+            max-width: 1210px;
+            margin: auto;
+            align-items: center;
+            display: block;
+            color: #161515;
+        }
+    `;
+
+    const AboutContent = styled.div`
+        margin: 14rem 0;
+    `;
+
+    const MarqueeContainer = styled.div`
+        // flex: 1 1 0;
+    `;
+    const AboutContentItem = styled.div`
+        padding: 2rem 0;
+    `;
+
     return (
         <>
-            <div className="about__heading ">No content here</div>
+            <SmoothScroll>
+                <AboutWrapperContainer>
+                    <AboutContent>
+                        <div className="about__heading">
+                            <SmoothScroll>
+                                <div className="relative about__headWrapper">
+                                    <h1 className="relative text-center">
+                                        About Me
+                                    </h1>
+                                </div>
+                            </SmoothScroll>
+                            <MarqueeContainer id="smooth-wrapper">
+                                <div className="marquee__headText ">
+                                    <Marquee>
+                                        <p className="marquee__text">
+                                            <ul className="flex flex-row">
+                                                <li>Let&apos;s Talk</li>
+                                                <li>UI/UX</li>
+                                                <li>React Js</li>
+                                                <li>Sass</li>
+                                                <li>Css</li>
+                                                <li>Figma</li>
+                                            </ul>
+                                        </p>
+                                    </Marquee>
+                                </div>
+                            </MarqueeContainer>
+                            <AboutContentItem>
+                                <div className="content">
+                                    <ThisContentAbout />
+                                </div>
+                            </AboutContentItem>
+                        </div>
+                    </AboutContent>
+                </AboutWrapperContainer>
+            </SmoothScroll>
+            <Footer />
         </>
     );
 };
